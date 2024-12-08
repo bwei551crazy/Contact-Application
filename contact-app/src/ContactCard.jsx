d5f1fd093e3b830b8dfb0b13759f7322781b5cb5
bwei551crazy <crazykwaibao@gmail.com>
2024-12-09 01:58:57 +1300

    Necessary features and colouring added

--- contact-app/src/ContactCard.jsx
+++ contact-app/src/ContactCard.jsx
@@ -4,13 +4,13 @@ const ContactCard = ({ contact }) => {
     <div className="card-restriction">
       <div className="card-body">
         <h5 className="card-title">{contact.name}</h5>
-        <p className="card-text">
+      </div>
+      <div className="card-text">
         <strong className="email"> Email:</strong> {contact.email}
         <br />
         <strong className="phone"> Phone:</strong> {contact.phone}
         <br />
         <strong className="website"> Website:</strong> <a href={`https://${contact.website}`} target="_blank" rel="noopener noreferrer">{contact.website}</a>
-        </p>
       </div>
     </div>
   );
